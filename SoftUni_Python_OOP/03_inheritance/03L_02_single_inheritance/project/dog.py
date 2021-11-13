cbbc27c414874ad2757e59d1b7f382e017f638ff
karalkal <karalkal@yahoo.co.uk>
2021-11-13 01:00:28 +0000

    added solution to lab task 2

---
+++ SoftUni_Python_OOP/03_inheritance/03L_02_single_inheritance/project/dog.py
@@ -0,0 +1,11 @@
+from project.animal import Animal
+
+class Dog(Animal):
+    def bark(self):
+        return "barking..."
+
+
+# ivan = Dog()
+# print(ivan.eat())
+# pesho = Dog()
+# print(pesho.bark())
