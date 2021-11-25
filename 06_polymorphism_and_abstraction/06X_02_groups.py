ad87182e6cd6921bba039aac5290f0c14822519e
karalkal <karalkal@yahoo.co.uk>
2021-11-25 15:17:49 +0000

    added solution to <Polymorphism and Abstraction> exrecise task 2

---
+++ 06_polymorphism_and_abstraction/06X_02_groups.py
@@ -0,0 +1,55 @@
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+    def __repr__(self):
+        return self.name + " " + self.surname
+        # option 2
+        # return f"{self.name} {self.surname}"
+
+
+class Group(Person):
+    def __init__(self, name, people):
+        self.name = name
+        self.people: list = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(self.name + " " + other.name, self.people + other.people)
+        # option 2
+        # return Group(f"{self.name} {other.name}", self.people + other.people)
+
+    def __repr__(self):
+        return f"Group {self.name} with members {', '.join(str(p) for p in self.people)}"
+
+    def __getitem__(self, index):
+        return f"Person {index}: {str(self.people[index])}"
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+# print(p4)
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+
+print(third_group)
+
+print(third_group[0])
+
+for person in third_group:
+    print(person)
