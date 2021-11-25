9e0a470e1051b1a15d96a8c009e5f929df6427e6
karalkal <karalkal@yahoo.co.uk>
2021-11-25 15:18:14 +0000

    added solution to <Polymorphism and Abstraction> exrecise task 3

---
+++ 06_polymorphism_and_abstraction/06X_03_account(no_static_method).py
@@ -0,0 +1,96 @@
+class Account:
+    def __init__(self, owner, amount=0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        else:
+            self._transactions.append(amount)
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    def validate_transaction(self, account, amount_to_add):
+        if account.balance + amount_to_add < 0:
+            raise ValueError("sorry cannot go in debt!")
+        else:
+            self.amount += amount_to_add
+            return f"New balance: {self.balance()}"
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, item):
+        return self._transactions[item]
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __ne__(self, other):
+        return self.balance != other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __add__(self, other):
+        created_account = Account(self.owner + "&" + other.owner, self.amount + other.amount)
+        for transaction in (self._transactions + other._transactions):  # combined list
+            created_account.add_transaction(transaction)
+        return created_account
+
+acc = Account('bob', 10)
+acc2 = Account('john')
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+
+for transaction in acc:
+    print(transaction)
+print()
+for transaction in acc2:
+    print(transaction)
+
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
+
+print()
+acc88 = acc + acc2
+print(acc88)
+# print(acc88.validate_transaction(acc88, -700000))
+print(acc88.validate_transaction(acc88, +700000))
