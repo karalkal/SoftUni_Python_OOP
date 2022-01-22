36902bd8606d19800e0afa6e335d5caffba754a6
karalkal <karalkal@yahoo.co.uk>
2022-01-22 20:38:52 +0000

    Added sports_car sub-class for tasks 1/2 from my Python OOP exam @ SoftUni

---
+++ MY_EXAM/task1_task2_simplified/project/car/sports_car.py
@@ -0,0 +1,14 @@
+from project.car.car import Car
+
+
+class SportsCar(Car):
+    min_speed_limit = 400
+    max_speed_limit = 600
+
+    def __init__(self, model: str, speed_limit: int):
+        super().__init__(model, speed_limit)
+
+
+# mc = SportsCar("SCar", 600)
+
+
